483c6367f6cd73e8a21954efa7c21b4349ec0e28
Sébastien Lamard <slamard@norsys.fr>
2025-02-03 17:35:11 +0100

    feat: continue tickets monitoring implementation

---
+++ src/components/SupportSection/assets/TicketInput.tsx
@@ -0,0 +1,26 @@
+import {useTranslation} from "react-i18next";
+import ButtonsJavaEdition from "../../utilities/ButtonsJavaEdition";
+import React from "react";
+
+type TicketContent = {
+    title: string,
+    email: string,
+    status: string
+};
+
+const TicketInput = (ticket: TicketContent, index: number) => {
+    const {t} = useTranslation();
+
+    function showOrHideSubMenu()
+    {
+    }
+
+    return (
+        <>
+            <input className="minecraft-input listed-ticket-input" id={`input${index}`} type="text" value={ticket.title} disabled/>
+            <ButtonsJavaEdition taille="square" title="+" onClick={() => showOrHideSubMenu()}/>
+        </>
+    );
+};
+
+export default TicketInput;
