b61af2055c5ae8a742a6250676a567ef6f5edae1
Sébastien Lamard <slamard@norsys.fr>
2025-03-05 15:43:15 +0100

    feat: tickets display

--- src/components/Admin/TicketsMonitoring.tsx
+++ src/components/Admin/TicketsMonitoring.tsx
@@ -1,16 +1,28 @@
 import {useTranslation} from "react-i18next";
-import React, {useEffect, useRef, useState} from "react";
+import React, {createRef, useEffect, useRef, useState} from "react";
 import "../../styles/Admin.css";
 import ButtonsJavaEdition from "../utilities/ButtonsJavaEdition";
 import axios from "axios";
 import {useNavigate} from "react-router-dom";
-import TicketInput, {TicketContent} from "../SupportSection/assets/TicketInput";
+
+export type TicketContent = {
+    id: string;
+    title: string,
+    email: string,
+    status: string
+    body: string
+};
 
 function TicketsMonitoring() {
     const {t} = useTranslation();
     const navigate = useNavigate();
     const [tickets, setTickets] = useState(new Array<TicketContent>());
     const isFirstLoad = useRef(true);
+    const ticketDetailsContainerRef = createRef<HTMLDivElement>();
+    const [selectedEmail, setSelectedEmail] = useState("");
+    const [selectedStatus, setSelectedStatus] = useState("");
+    const [selectedSubject, setSelectedSubject] = useState("");
+    const [selectedBody, setSelectedBody] = useState("");
 
     function isTicketRegistered(ticket: TicketContent): boolean {
         return tickets.filter((item) => item.id === ticket.id).length > 0;
@@ -36,9 +48,10 @@ function TicketsMonitoring() {
                         title: element.titre,
                         email: element.email,
                         status: element.statut,
+                        body: element.corps,
                     };
 
-                    if (isTicketRegistered(ticketContent)) {
+                    if (isTicketRegistered(ticketContent) || element.archived) {
                         return;
                     }
 
@@ -50,23 +63,46 @@ function TicketsMonitoring() {
         }
     }, [tickets]);
 
+    function showTicketDetails(ticket: TicketContent) {
+        setSelectedEmail(ticket.email);
+        setSelectedStatus(ticket.status);
+        setSelectedSubject(ticket.title);
+        setSelectedBody(ticket.body);
+        ticketDetailsContainerRef.current?.removeAttribute("hidden");
+    }
+
     return (
-        <div className="admin-page">
-            <h1 className="admin-title">{t("ADMIN.TICKETS")}</h1>
+        <div className="admin-tickets-page">
+            <h1 className="admin-title">{t("ADMIN.TICKETS.TITLE")}</h1>
             <div className="admin">
-                <div className="admin-menu">
-                    <ul className="admin-menu-list">
-                        {tickets.map((ticket: TicketContent, index: number) => (
-                            <li key={index}><TicketInput
-                                ticket={ticket}
-                                index={index}
-                            ></TicketInput></li>
-                        ))}
-                    </ul>
+                <div className="admin-tickets-list">
+                    {tickets.map((ticket: TicketContent) => (
+                        <div className="admin-ticket-elements">
+                            <ButtonsJavaEdition taille="28" title={ticket.title} onClick={() => showTicketDetails(ticket)}/>
+                        </div>
+                    ))}
+                </div>
+                <div ref={ticketDetailsContainerRef} className="admin-ticket-display" hidden>
+                    <div className="admin-ticket-detail">
+                        <label htmlFor="ticket-email">{t("ADMIN.TICKETS.EMAIL") + ':'}</label>
+                        <p id="ticket-email">{selectedEmail}</p>
+                    </div>
+                    <div className="admin-ticket-detail">
+                        <label htmlFor="ticket-status">{t("ADMIN.TICKETS.STATUS") + ':'}</label>
+                        <p id="ticket-status">{selectedStatus}</p>
+                    </div>
+                    <div className="admin-ticket-detail">
+                        <label htmlFor="ticket-title">{t("ADMIN.TICKETS.SUBJECT") + ':'}</label>
+                        <p id="ticket-title">{selectedSubject}</p>
+                    </div>
+                    <div className="admin-ticket-body">
+                        <label htmlFor="ticket-body">{t("ADMIN.TICKETS.BODY") + ':'}</label>
+                        <p id="ticket-body">{selectedBody}</p>
+                    </div>
                 </div>
             </div>
             <div className="admin-buttons">
-                <ButtonsJavaEdition taille="40" title={t("GLOBAL.BACK")} path="goback"/>
+                <ButtonsJavaEdition taille="80" title={t("GLOBAL.BACK")} path="goback"/>
             </div>
         </div>
     );
